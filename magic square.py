aa86c9565ca94787d208a9e1616ff9d21cf6d013
jainj2305 <53536106+jainj2305@users.noreply.github.com>
2020-06-16 17:16:24 +0530

    Add files via upload

---
+++ magic square.py
@@ -0,0 +1,48 @@
+def magic_square(n):
+    square=[]
+    for i in range(n):
+        l=[]
+        for j in range(n):
+            l.append(0)
+        square.append(l)
+    i=n//2
+    j=n-1
+    num=n*n
+    count=1
+    
+    while(count<=num):
+        if(i==-1 and j==n):
+            i=0
+            j=n-2
+        else:
+            if(j==n):
+                j=0
+            if(i<0):
+                i=n-1
+        if(square[i][j]!=0):
+            j=j-2
+            i=i+1
+            continue
+        else:
+            square[i][j]=count
+            count+=1
+        i=i-1
+        j=j+1
+        
+    for i in range(n):
+        for j in range(n):
+            print(square[i][j],end=" ")
+        print()
+
+def run():
+    while True:
+        x=int(input("Enter any odd no.: "))
+        if(x%2==0):
+            print("Sorry it's magic square is not possible!!\nPlease enter a odd no.:")
+        else:
+            magic_square(x)
+            break
+
+
+if __name__ == '__main__' :
+    run()
